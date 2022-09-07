31465517a6d878605da05349fc38eb1711b6491e
Nagehan54 <nagehanaydin@yahoo.com>
2022-09-07 13:48:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let addtoDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputText = document.getElementById('inputText')
+let cleartodo = document.getElementById('clearToDo')
+
+addtoDoButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling')
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerHTML = inputText.value;
+    inputText.value = "";
+   
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = 'line-through';
+      
+    });
+    paragraph.addEventListener('dblclick', function(){
+      toDoContainer.removeChild(paragraph);
+
+    });
+
+    cleartodo.addEventListener('click', function(){
+      paragraph.remove();
+
+    })
+
+});
